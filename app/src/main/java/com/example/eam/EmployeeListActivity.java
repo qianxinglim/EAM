94e661345289f3926be2ea97995491d39c565082
qianxinglim <64999593+qianxinglim@users.noreply.github.com>
2021-09-05 06:27:58 +0800

    - EmployeeListActivity added

--- app/src/main/java/com/example/eam/EmployeeListActivity.java
+++ app/src/main/java/com/example/eam/EmployeeListActivity.java
@@ -1,14 +1,119 @@
 package com.example.eam;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.eam.databinding.ActivityEmployeeListBinding;
+import com.example.eam.menu.AdminFragment;
+import com.example.eam.menu.AdminListFragment;
+import com.example.eam.menu.ChatsFragment;
+import com.example.eam.menu.EmployeeListFragment;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class EmployeeListActivity extends AppCompatActivity {
+
+    private ActivityEmployeeListBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_employee_list);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_employee_list);
+
+        setUpWithViewPager(binding.viewPager);
+        binding.tabLayout.setupWithViewPager(binding.viewPager);
+        setSupportActionBar(binding.toolbar);
+
+        binding.fabAction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(EmployeeListActivity.this, "Add Employees", Toast.LENGTH_SHORT).show();
+                //startActivity(new Intent(EmployeeListActivity.this, ContactsActivity.class));
+            }
+        });
+
+        binding.viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                performOnClick(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
     }
+
+    private void setUpWithViewPager(ViewPager viewPager) {
+        EmployeeListActivity.SectionsPagerAdapter adapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new EmployeeListFragment(),"Users");
+        adapter.addFragment(new AdminListFragment(),"Admins");
+        viewPager.setAdapter(adapter);
+    }
+
+    private static class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public SectionsPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+    }
+
+    private void performOnClick(final int index) {
+        binding.fabAction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (index==0){
+                    Toast.makeText(EmployeeListActivity.this, "Add Employee", Toast.LENGTH_SHORT).show();
+                    //startActivity(new Intent(EmployeeListActivity.this, ContactsActivity.class));
+                }else if (index==1){
+                    Toast.makeText(EmployeeListActivity.this, "Add Admin", Toast.LENGTH_SHORT).show();
+                    //checkCameraPermission();
+                }
+            }
+        });
+    }
+
+
 }
---
+++ app/src/main/java/com/example/eam/menu/EmployeeListFragment.java
@@ -0,0 +1,25 @@
+package com.example.eam.menu;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.eam.R;
+
+public class EmployeeListFragment extends Fragment {
+
+    public EmployeeListFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_employee_list, container, false);
+    }
+}
